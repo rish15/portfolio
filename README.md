834dc9506809a83b66af0635a2711d8bd4cbad39
rish15 <rish.cr7mini@gmail.com>
2021-04-18 15:56:46 +0530

    fixes

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# portfolio
-Personal site
+[![Netlify Status](https://api.netlify.com/api/v1/badges/7f112e78-9c3e-4304-a421-1210b38e474b/deploy-status)](https://app.netlify.com/sites/rishushrivastav/deploys)
---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+ *function checkForWindowResize() {
+ *  console.log(`Screen width: ${window.innerWidth}`);
+ *
+ *  if (window.innerWidth < 600) {
+ *    const element = document.getElementsByClassName("container-fluid");
+ *    element[0].classList.remove("container-fluid");
+ *  } else {
+ *    const element = document.getElementById("main");
+ *    element.classList.add("container-fluid");
+ *  }
+ *}
+ *
+ *window.addEventListener("resize", checkForWindowResize);
+ */
