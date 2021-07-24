fc8302143acc6ab1ee4abb27d1e23528830b21fb
rish15 <rish.cr7mini@gmail.com>
2021-07-24 14:55:53 +0530

    some changes

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,9 +1,8 @@
 export async function handler(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello world ${Math.floor(Math.random() * 10)}`,
-    }),
-  };
-  lmdalsmd;
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Hello world ${Math.floor(Math.random() * 10)}`,
+        }),
+    };
 }
